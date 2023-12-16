0ea924b8b8c64795ab4010b07c27d52abadd770d
Zhicheng Xiong <zhichengx@ipp2-1782.nvidia.com>
2023-12-15 20:45:32 -0800

    fix to be able to run

--- src/models/student.py
+++ src/models/student.py
@@ -10,6 +10,8 @@ from utils import get_sep_position
 from .configuration_student import StudentConfig
 from .modeling_gpt2_implicit import GPT2LMHeadImplicitModel
 
+import logging
+
 class Student(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -91,7 +93,11 @@ class Student(nn.Module):
         config = StudentConfig.from_pretrained(pretrained_path)
         model = Student(config)
         state_dict = torch.load(os.path.join(pretrained_path, 'state_dict.bin'))
-        model.load_state_dict(state_dict)
+        try:
+            model.load_state_dict(state_dict)
+        except:
+            model.load_state_dict(state_dict, strict=False)
+            logging.warn("Some weights of the model Student checkpoint not loaded.")
         return model
 
     def save_pretrained(self, save_directory):
